fa1f9e344076073e25387989bf5d61e960c09340
achan <achan@achans-MacBook-Pro.local>
2021-06-15 15:31:47 -0700

    fixing typo in deploy.yml; deleting unecessary methods from tasks.py and services.py

--- LotDividerAPI/tasks.py
+++ LotDividerAPI/tasks.py
@@ -4,9 +4,4 @@ from datetime import date
 
 @app.task
 def getClosingData(closingDate=date.today().strftime("%Y-%m-%d")):
-    print('huh')
     queries.getClosingPrices()
-
-@app.task
-def add(x,y):
-    return x+y
